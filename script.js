807967ad777bc3152d96c846a642cbf05b2f4230
Jaes98 <marcus.jaes@gmail.com>
2024-11-18 01:29:02 +0100

    enemy moving, player moving, boundries work, no collision yet or animaitons

--- script.js
+++ script.js
@@ -1,80 +1,115 @@
-window.addEventListener('load', start);
-
+window.addEventListener("load", start);
 
 const controls = {
-    up: false,
-    down: false,
-    left: false,
-    right: false
+  up: false,
+  down: false,
+  left: false,
+  right: false,
 };
 
 const player = {
-    x: 0,
-    y: 0,
-    width: 32,
-    height: 40,
-    speed: 20
+  x: 0,
+  y: 0,
+  width: 32,
+  height: 40,
+  speed: 70,
 };
 
 const enemy = {
-    x: 0,
-    y: 0,
-    width: 32,
-    height: 32,
-    speed: 10
+  x: 0,
+  y: 200,
+  width: 32,
+  height: 32,
+  speed: 2,
+  direction: 1,
 };
 
 const gameBoard = {
-    left: 0,
-    top: 0,
-    right: 400,
-    bottom: 400,
+  left: 0,
+  top: 0,
+  right: 400,
+  bottom: 400,
 };
 
-const playerElement = document.getElementById('player');
-const gameBoardElement = document.getElementById('gameBoard');
+const playerElement = document.getElementById("player");
+const gameBoardElement = document.getElementById("gameBoard");
 
 function start() {
-    console.log('js running');
+  console.log("js running");
 
-    window.addEventListener('keydown', keyPressed);
-    window.addEventListener('keyup', keyPressed);
+  window.addEventListener("keydown", keyPressed);
+  window.addEventListener("keyup", keyPressed);
 
-
-    requestAnimationFrame(tick);
-};
-
-
+  requestAnimationFrame(tick);
+}
 
 let prevTime = 0;
 function tick(time) {
   requestAnimationFrame(tick);
 
-  const deltaTime = time - prevTime;
+  const deltaTime = (time - prevTime) / 1000;
   prevTime = time;
 
-//   movePlayer(deltaTime);
-//   moveEnemy(deltaTime);
   displayPlayer();
+  movePlayer(deltaTime);
+  moveEnemy(deltaTime);
 }
 
+function movePlayer(deltaTime) {
+  const newPosition = {
+    x: player.x,
+    y: player.y,
+  };
 
+  if (controls.left) {
+    newPosition.x -= player.speed * deltaTime;
+  }
+  if (controls.right) {
+    newPosition.x += player.speed * deltaTime;
+  }
+  if (controls.up) {
+    newPosition.y -= player.speed * deltaTime;
+  }
+  if (controls.down) {
+    newPosition.y += player.speed * deltaTime;
+  }
 
+  if (canMove(newPosition)) {
+    player.x = newPosition.x;
+    player.y = newPosition.y;
+  }
+}
 
-// function displayPlayer() {
-//   if (controls.up || controls.down || controls.left || controls.right) {
-//     playerElement.style.translate = `${playerInfo.x}px ${playerInfo.y}px`;
-//   }
-//   playerElement.style.backgroundPosition = `${animationStuff.walkDir * 100}% ${
-//     animationStuff.characterDirection * 100
-//   }%`;
-// }
+function moveEnemy(deltaTime) {
+  enemy.x += enemy.speed * enemy.direction;
+
+  if (enemy.x <= gameBoard.left || enemy.x + enemy.width >= gameBoard.right) {
+    enemy.direction *= -1;
+  }
+
+  const enemyElement = document.getElementById("enemy");
+  enemyElement.style.transform = `translate(${enemy.x}px, ${enemy.y}px)`;
+}
 
 function displayPlayer() {
-    player.y = 40;
-    player.x = 40;
+  playerElement.style.translate = `${player.x}px ${player.y}px`;
+}
 
-    playerElement.style.transform = `translate(${player.x}px, ${player.y}px)`;
+function canMove(newPosition) {
+  if (
+    newPosition.x < gameBoard.left ||
+    newPosition.x + player.width > gameBoard.right
+  ) {
+    return false;
+  }
+  if (
+    newPosition.y < gameBoard.top ||
+    newPosition.y + player.height > gameBoard.bottom
+  ) {
+    return false;
+  }
+
+  return true;
 }
 
 function keyPressed(event) {
@@ -91,4 +126,3 @@ function keyPressed(event) {
     controls.right = boolValue;
   }
 }
-
